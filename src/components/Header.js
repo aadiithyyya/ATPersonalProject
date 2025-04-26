2722f9937e98807ba3d02968b6b0071d56260685
Aadithya Ram <aadithyaram@Aadithyas-MacBook-Air.local>
2025-04-26 18:25:27 +0530

    trd

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <h1>Aadi made this for thara 🥰 bcs he is jobless (he actually got an extremly good offer and he's waiting for the joinig date but he cant go on without coding)</h1>
-      <p>A cute jilo site made by <a href="https://linkedin.com/in/aadiithyyya" target="_blank" rel="noopener noreferrer">Aadi</a></p>
-      </header>
+      <h1>Aadi made this for Thara BBY🥰</h1>
+      <p>(He is very jobless rn💀 but he actually got an extremely good offer and is waiting for the joining date but can't stay without coding🤠)</p>
+      <p>A cute jilo site ~ with love <a href="https://linkedin.com/in/aadiithyyya" target="_blank" rel="noopener noreferrer">Aadi</a></p>
+    </header>
   );
 }
 
